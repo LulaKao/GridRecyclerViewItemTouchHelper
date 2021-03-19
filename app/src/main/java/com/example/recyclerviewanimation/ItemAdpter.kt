e811dcbdd9b82f30b09d8ad075bad6edf92dd766
lula <lula@mitake.com.tw>
2021-03-19 18:26:39 +0800

    add onKeyListener and focus

--- app/src/main/java/com/example/recyclerviewanimation/ItemAdpter.kt
+++ app/src/main/java/com/example/recyclerviewanimation/ItemAdpter.kt
@@ -1,7 +1,10 @@
 package com.example.recyclerviewanimation
 
+import android.graphics.Color
 import android.util.Log
+import android.view.KeyEvent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewanimation.databinding.ItemCardBinding
@@ -10,6 +13,7 @@ import kotlin.collections.ArrayList
 
 class ItemAdapter(private val dataList: ArrayList<ItemModel>) :
     RecyclerView.Adapter<ItemAdapter.ViewHolder>(), ItemHelperInterface {
+    var index : Int = 0
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemBinding = ItemCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -24,13 +28,42 @@ class ItemAdapter(private val dataList: ArrayList<ItemModel>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.d("PPP", "onBindViewHolder $position")
         val itemModel: ItemModel = dataList[position]
         holder.bind(itemModel)
+        holder.itemView.isFocusable = true
+        holder.itemView.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if(hasFocus) holder.itemView.setBackgroundColor(Color.YELLOW)
+            else holder.itemView.setBackgroundColor(Color.GRAY)
+            Log.d("PPP", "onFocusChangeListener $hasFocus,$position")
+        }
+        holder.itemView.setOnKeyListener(object :View.OnKeyListener{
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+                if(event?.action!= null){
+                    if(event.action ==KeyEvent.ACTION_DOWN && keyCode ==KeyEvent.KEYCODE_DPAD_DOWN){
+                        onItemVerticalMove(position,position+5)
+                        index = position+5
+                        notifyDataSetChanged() // handler 等一秒再做，看可否看到動畫
+                        return true
+                    } else if(event.action ==KeyEvent.ACTION_DOWN && keyCode ==KeyEvent.KEYCODE_DPAD_UP){
+                        onItemVerticalMove(position,position-5)
+                        index = position-5
+                        notifyDataSetChanged() // handler 等一秒再做，看可否看到動畫
+                        return true
+                    }
+                }
+                return false
+            }
+        })
+        if(index == position) holder.itemView.requestFocus() // 只有一個 focus
     }
 
     override fun getItemCount() = dataList.size
 
     override fun onItemVerticalMove(fromPosition: Int, toPosition: Int) {
+//        var itemSelected = dataList[fromPosition]
+//        dataList.removeAt(fromPosition)
+//        dataList.add(toPosition, itemSelected)
         Collections.swap(dataList,fromPosition,toPosition)
         notifyItemMoved(fromPosition,toPosition)
         Log.d("test", "onItemVerticalMove ===")
